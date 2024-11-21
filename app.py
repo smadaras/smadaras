2b9b8fc4a729a93950b81f5a8e2f28ccf6b9a3f1
Sandor Madaras <sandor.madaras@gmail.com>
2024-11-22 00:15:05 +0100

    caret

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from js import document
      
 def main():
     msg = document.getElementById("msg")
-    msg.innerHTML = 'Hello world with static variable'
+    msg.innerHTML = 'Hello world with static variable caret'
     print(f"Current working directory: {os.getcwd()}")
     print(f"Environment: {os.getenv('ENV_VAR')}")
        
