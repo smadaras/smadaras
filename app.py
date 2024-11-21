1b28516c64bdac51ccbb1be7bf785fba55a38889
Sandor Madaras <sandor.madaras@gmail.com>
2024-11-22 00:20:40 +0100

    pinned workflow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from js import document
      
 def main():
     msg = document.getElementById("msg")
-    msg.innerHTML = 'Hello world with static variable caret'
+    msg.innerHTML = 'Hello world with pinned workflow'
     print(f"Current working directory: {os.getcwd()}")
     print(f"Environment: {os.getenv('ENV_VAR')}")
        
