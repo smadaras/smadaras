9de269851fb816ed43698222c3cafc302df8f1cb
Sandor Madaras <sandor.madaras@gmail.com>
2024-11-22 02:19:12 +0100

    joblevel

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from js import document
      
 def main():
     msg = document.getElementById("msg")
-    msg.innerHTML = 'Hello world alert envfix3'
+    msg.innerHTML = 'Hello world alert joblevel'
     print(f"Current working directory: {os.getcwd()}")
     print(f"Environment: {os.getenv('ENV_VAR')}")
     print(f"Static: {os.getenv('url')}")
