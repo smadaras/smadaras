8538619d627a3bb9180868e7f48ae716b56fe262
Sandor Madaras <sandor.madaras@gmail.com>
2024-11-22 02:01:40 +0100

    alert box

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from js import document
      
 def main():
     msg = document.getElementById("msg")
-    msg.innerHTML = 'Hello world with html env'
+    msg.innerHTML = 'Hello world alert box'
     print(f"Current working directory: {os.getcwd()}")
     print(f"Environment: {os.getenv('ENV_VAR')}")
     print(f"Static: {os.getenv('url')}")
