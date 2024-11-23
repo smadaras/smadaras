ee71e2e0021074a54ca0b2463c8587767dc9d266
smadaras <76752573+smadaras@users.noreply.github.com>
2024-11-23 03:57:10 +0000

    secret

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     msg = document.getElementById("msg")
     msg.innerHTML = 'Hello world alert secret'
     print(f"Current working directory: {os.getcwd()}")
-    print(f"Environment: {os.getenv('ENV_VAR')}")
+    print(f"Environment: {os.getenv('ENV_SEC')}")
     print(f"Static: {os.getenv('url')}")
        
 main()
